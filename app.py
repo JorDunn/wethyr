a4025b2c75b3fe525040de8f677660d93cd4e73a
Jordan Dunn <jordan@nodetwo.net>
2014-01-10 00:41:18 -0600

    Refactored general code into Wethyr class. Coordinate system works perfectly. Upcoming updates will be mostly user and ui centric

--- app.py
+++ app.py
@@ -6,27 +6,43 @@ import json, requests, dis, operator, time
 
 app = Flask(__name__)
 
-app.debug = False
+app.debug = True
 
-def degreesToCardinal(deg):
-	if(((operator.gt(deg, 326)) and (operator.lt(deg, 360))) or ((operator.gt(deg, 0)) and (operator.lt(deg, 12)))):
-		return 'North'
-	elif((operator.gt(deg, 11)) and (operator.lt(deg, 57))):
-		return 'Northeast'
-	elif((operator.gt(deg, 56)) and (operator.lt(deg, 102))):
-		return 'East'
-	elif((operator.gt(deg, 101)) and (operator.lt(deg, 169))):
-		return 'Southeast'
-	elif((operator.gt(deg, 168)) and (operator.lt(deg, 192))):
-		return 'South'
-	elif((operator.gt(deg, '191')) and (operator.lt(deg, 237))):
-		return 'Southwest'
-	elif((operator.gt(deg, 236)) and (operator.lt(deg, 282))):
-		return 'West'
-	elif((operator.gt(deg, 281)) and (operator.lt(deg, 327))):
-		return 'Northwest'
-	else:
-		return 'Unknown'
+class Wethyr(object):
+
+	def degreesToCardinal(self, deg):
+		if(((operator.gt(deg, 326)) and (operator.lt(deg, 360))) or ((operator.gt(deg, 0)) and (operator.lt(deg, 12)))):
+			return 'North'
+		elif((operator.gt(deg, 11)) and (operator.lt(deg, 57))):
+			return 'Northeast'
+		elif((operator.gt(deg, 56)) and (operator.lt(deg, 102))):
+			return 'East'
+		elif((operator.gt(deg, 101)) and (operator.lt(deg, 169))):
+			return 'Southeast'
+		elif((operator.gt(deg, 168)) and (operator.lt(deg, 192))):
+			return 'South'
+		elif((operator.gt(deg, '191')) and (operator.lt(deg, 237))):
+			return 'Southwest'
+		elif((operator.gt(deg, 236)) and (operator.lt(deg, 282))):
+			return 'West'
+		elif((operator.gt(deg, 281)) and (operator.lt(deg, 327))):
+			return 'Northwest'
+		else:
+			return 'Unknown'
+
+	def processWeatherInfo(self, weather_json, units):
+		self.weather = { 'temp': int(weather_json['main']['temp']),
+						 'humidity': weather_json['main']['humidity'],
+						 'pressure': weather_json['main']['pressure'],
+						 'wind_speed': weather_json['wind']['speed'],
+						 'wind_direction': self.degreesToCardinal(weather_json['wind']['deg']), 
+						 'sky': str(weather_json['weather'][0]['main']).lower(), 
+						 'sky_desc': str(weather_json['weather'][0]['description']).title(), 
+						 'city': str(weather_json['name']).title(), 
+						 'country': str(weather_json['sys']['country']).title(),
+		}
+
+		return self.weather
 
 @app.route('/', methods=['GET', 'POST'])
 def getWeatherDefault(city='Shakopee', country='US', units='imperial'):
@@ -36,106 +52,76 @@ def getWeatherDefault(city='Shakopee', country='US', units='imperial'):
 		url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=' + units
 
 		res = requests.get(url)
-		weather = json.loads(json.dumps(res.json()))
+		weather_json = json.loads(json.dumps(res.json()))
 
-		temp     = int(weather['main']['temp'])
-		temp_hi  = weather['main']['temp_max']
-		temp_low = weather['main']['temp_min']
+		wethyr = Wethyr()
+		weather = wethyr.processWeatherInfo(weather_json, units)
 
-		humidity = weather['main']['humidity']
-		pressure = weather['main']['pressure']
-
-		wind_speed = weather['wind']['speed']
-		wind_direction = degreesToCardinal(weather['wind']['deg'])
-
-		sky      = str(weather['weather'][0]['main']).lower()
-		sky_desc = str(weather['weather'][0]['description']).title()
-
-		if(units == 'imperial'):
+		if (units == 'imperial'):
 			flag = 'F'
 		else:
 			flag = 'C'
 
-		if (sky == 'clear'):
-			if ((int(time.time()) > int(weather['sys']['sunrise'])) and (int(time.time() < int(weather['sys']['sunset'])))):
+		sky = weather['sky']
+		if (weather['sky'] == 'clear'):
+			if ((int(time.time()) > int(weather_json['sys']['sunrise'])) and (int(time.time() < int(weather_json['sys']['sunset'])))):
 				sky = 'clear_day'
 			else:
 				sky = 'clear_night'
 
-		return render_template('index.html', city=city, country=country, temp=temp, temp_hi=temp_hi, temp_low=temp_low, humidity=humidity, pressure=pressure, wind_speed=wind_speed, wind_direction=wind_direction, sky=sky, sky_desc=sky_desc, flag=flag)
+		return render_template('index.html', city=weather['city'], country=weather['country'], temp=weather['temp'], humidity=weather['humidity'], pressure=weather['pressure'], wind_speed=weather['wind_speed'], wind_direction=weather['wind_direction'], sky=sky, sky_desc=weather['sky_desc'], flag=flag)
 
 @app.route('/<city>/<country>/<units>', methods=['GET'])
 def getWeatherByCity(city='Shakopee', country='US', units='imperial'):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=' + units
 
 	res = requests.get(url)
-	weather = json.loads(json.dumps(res.json()))
+	weather_json = json.loads(json.dumps(res.json()))
 
-	temp     = int(weather['main']['temp'])
-	temp_hi  = weather['main']['temp_max']
-	temp_low = weather['main']['temp_min']
-
-	humidity = weather['main']['humidity']
-	pressure = weather['main']['pressure']
-
-	wind_speed = weather['wind']['speed']
-	wind_direction = degreesToCardinal(weather['wind']['deg'])
-
-	sky      = str(weather['weather'][0]['main']).lower()
-	sky_desc = str(weather['weather'][0]['description']).title()
+	wethyr = Wethyr()
+	weather = wethyr.processWeatherInfo(weather_json, units)
 
 	if (units == 'imperial'):
 		flag = 'F'
 	else:
 		flag = 'C'
 
-	if (sky == 'clear'):
-		if ((int(time.time()) > int(weather['sys']['sunrise'])) and (int(time.time() < int(weather['sys']['sunset'])))):
+	sky = weather['sky']
+	if (weather['sky'] == 'clear'):
+		if ((int(time.time()) > int(weather_json['sys']['sunrise'])) and (int(time.time() < int(weather_json['sys']['sunset'])))):
 			sky = 'clear_day'
 		else:
 			sky = 'clear_night'
 
-	return render_template('index.html', city=city, country=country, temp=temp, temp_hi=temp_hi, temp_low=temp_low, humidity=humidity, pressure=pressure, wind_speed=wind_speed, wind_direction=wind_direction, sky=sky, sky_desc=sky_desc, flag=flag)
+	return render_template('index.html', city=weather['city'], country=weather['country'], temp=weather['temp'], humidity=weather['humidity'], pressure=weather['pressure'], wind_speed=weather['wind_speed'], wind_direction=weather['wind_direction'], sky=sky, sky_desc=weather['sky_desc'], flag=flag)
 
-@app.route('/lat/<lat>/lon/<lon>/<units>')
+@app.route('/lat/<lat>/lon/<lon>/<units>', methods=['GET'])
 def getWeatherByCoords(lat, lon, units='imperial'):
 	url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + units
 
-	res  = requests.get(url)
-	weather = json.loads(json.dumps(res.json()))
+	res = requests.get(url)
+	weather_json = json.loads(json.dumps(res.json()))
 
-	temp     = int(weather['main']['temp'])
-	temp_hi  = weather['main']['temp_max']
-	temp_low = weather['main']['temp_min']
+	wethyr = Wethyr()
+	weather = wethyr.processWeatherInfo(weather_json, units)
 
-	humidity = weather['main']['humidity']
-	pressure = weather['main']['pressure']
-
-	wind_speed = weather['wind']['speed']
-	wind_direction = degreesToCardinal(weather['wind']['deg'])
-
-	sky      = str(weather['weather'][0]['main']).lower()
-	sky_desc = str(weather['weather'][0]['description']).title()
-
-	city = weather['name']
-	country = weather['sys']['country']
-
-	if(units == 'imperial'):
+	if (units == 'imperial'):
 		flag = 'F'
 	else:
 		flag = 'C'
 
-	if (sky == 'clear'):
-		if ((int(time.time()) > int(weather['sys']['sunrise'])) and (int(time.time() < int(weather['sys']['sunset'])))):
+	sky = weather['sky']
+	if (weather['sky'] == 'clear'):
+		if ((int(time.time()) > int(weather_json['sys']['sunrise'])) and (int(time.time() < int(weather_json['sys']['sunset'])))):
 			sky = 'clear_day'
 		else:
 			sky = 'clear_night'
 
-	return render_template('index.html', city=city, country=country, temp=temp, temp_hi=temp_hi, temp_low=temp_low, humidity=humidity, pressure=pressure, wind_speed=wind_speed, wind_direction=wind_direction, sky=sky, sky_desc=sky_desc, flag=flag)
+	return render_template('index.html', city=weather['city'], country=weather['country'], temp=weather['temp'], humidity=weather['humidity'], pressure=weather['pressure'], wind_speed=weather['wind_speed'], wind_direction=weather['wind_direction'], sky=sky, sky_desc=weather['sky_desc'], flag=flag)
 
 @app.route('/coords')
 def getCoords():
 	return render_template('coords.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run('0.0.0.0', 7070)
